4695b62460b953db5e4c2f414e82c08a649940de
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-01-22 15:20:54 -0300

    feat(statement): created get statemente function

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ app.use(express.json());
 
 const customers = [];
 
-/*
- * cpf - string 
- * name - string
- * id - uuid
- * statement []
- */
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
@@ -32,6 +26,18 @@ app.post("/account", (request, response) => {
 
     return response.status(201).send();
 
+});
+
+app.get("/statement", (request, response) => {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ Error: "Customer not found" });
+    }
+
+    return response.json(customer.statement);
 });
 
 app.listen(3333);
